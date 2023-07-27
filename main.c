472b5d17015b7b9e5d5ac2f73e59583fe1053193
shikadia <otukachinedu661@gmail.com>
2023-07-27 04:59:05 -0700

    update unknow

--- main.c
+++ main.c
@@ -9,44 +9,7 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Can you print an address?\n%p\nNice!\n", (void *)0x7fff5100b6f8);
-    printf("Can you print an address?\n%p\nNice!\n", (void *)0x7fff5100b6f8);
-    _printf("%p\n", addr);
-    printf("%p\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len2);
-    printf("Len:[%d]\n", len2);
-    _printf("%R", "Guvf fragrapr vf ergevrirq sebz in_netf!\n");
-    _printf("Complete the sentence: You nothing, Jon Snow.\n");
-    _printf("Complete the sentence: You %r nothing, Jon Snow.\n", "");
-    _printf("Complete the sentence: You %R nothing, Jon Snow.\n", "xabj");
-    _printf("Complete the sentence: You %R nothing, Jon Snow.\n", "");
-    printf("%R", "Guvf fragrapr vf ergevrirq sebz in_netf!\n");
-    _printf("Complete the sentence: You %r nothing, Jon Snow.\n", "know");
-    _printf("Complete the sentence: You %r nothing, Jon Snow.\n", "");
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
     return (0);
 }
